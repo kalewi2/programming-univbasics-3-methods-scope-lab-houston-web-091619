086af710d1ad89a278825fc642331b66f290e3ae
Kim Lewis <kalewi2@gmail.com>
2019-08-28 19:42:26 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,13 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  take_this = "It's Dangerous To Go Alone! Take This."
+  puts take_this
+end
+
+def any_phrase
+  roll = "Do A Barrel Roll!"
+  puts roll
+end
